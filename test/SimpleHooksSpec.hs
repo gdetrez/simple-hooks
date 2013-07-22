d01221477133801e652a69fcc8b786da5503fd01
Grégoire Détrez <gregoire.detrez@gu.se>
2013-07-22 19:38:04 +0200

    Add license headers

--- test/SimpleHooksSpec.hs
+++ test/SimpleHooksSpec.hs
@@ -1,6 +1,22 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ExtendedDefaultRules #-}
 {-# OPTIONS_GHC -fno-warn-type-defaults #-}
+-- simple-hooks: an easier way to define git hooks
+-- Copyright (C) 2013 Grégoire Détrez
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see [http://www.gnu.org/licenses/].
+
 module SimpleHooksSpec where
 
 import SimpleHooks -- SUT
